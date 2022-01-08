61129a83cbd22073ba5fb5a4c89724252276c8bd
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-08 13:53:15 +0100

    delete method

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -19,7 +19,17 @@ const createTask = async (req, res) => {
     }
 };
 
-const getTask = (req, res) => {
+const getTask = async(req, res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
   res.json({ id: req.params.id });
 };
 
@@ -27,8 +37,18 @@ const updateTask = (req, res) => {
   res.send('update a task');
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete a task');
+const deleteTask = async(req, res) => {
+    try{
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    }catch(error){
+        res.status(500).json({msg:error})
+    }
+  
 };
 
 module.exports = {
