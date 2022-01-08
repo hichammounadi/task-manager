575958683e2729017497ad623515e58712e62c3c
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-08 16:01:13 +0100

    asyncWrapper error handler => basics concepts

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
 // setting the port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(express.static('./public'))
@@ -12,7 +15,8 @@ app.use(express.json());
 
 //ROUTES
 app.use('/api/v1/tasks', tasks);
-
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
